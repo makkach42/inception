6691b187a008ad6a1b8251ee576da2e96712bb2c
makkach42 <189625447+makkach42@users.noreply.github.com>
2026-07-03 12:46:06 -0400

    redis finished

--- retry/srcs/requirements/wordpress/script.sh
+++ retry/srcs/requirements/wordpress/script.sh
@@ -14,7 +14,7 @@ if ! command -v wp > /dev/null 2>&1 ; then
     wp_sec_password=$(cat /run/secrets/WP_SEC_USER_PASS)
     admin_password=$(cat /run/secrets/ADMIN_USER_PASS)
 
-    wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=mariadb --dbpass=$db_password --path=/wordpress --extra-php --allow-root <<PHP
+    wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=mariadb --dbpass=$db_password --path=/wordpress  --allow-root --extra-php <<PHP
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 PHP
@@ -23,11 +23,11 @@ PHP
 
     wp user create ${WP_SEC_USER} ${WP_SEC_USER_MAIL} --user_pass=${wp_sec_password}  --path=/wordpress --allow-root
 
-    wp plugin install redis-cache --allow-root
+    wp plugin install redis-cache --allow-root --path=/wordpress
 
-    wp plugin activate redis-cache --allow-root
+    wp plugin activate redis-cache --allow-root --path=/wordpress
 
-    wp redis start --allow-root
+    wp redis enable --allow-root --path=/wordpress
 
 fi
 
